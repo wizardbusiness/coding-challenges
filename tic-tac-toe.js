88e66d3490bec01e1f2dba9523ccdd5c6ae13166
gkime <gkime@practice.com>
2022-04-23 18:42:34 -0700

    laid out rules for challenge, created function  with blank body.

---
+++ tic-tac-toe.js
@@ -0,0 +1,14 @@
+
+/* Find a winner in a tic-tac-toe game.
+
+The first player A always places 'X' characters,
+while the second player B always places 'O' characters.
+
+Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that
+the ith move will be played on grid[rowi][coli]. return the winner of the
+game if it exists (A or B). In case the game ends in a draw return "Draw". If
+there are still movements to play return "Pending". */
+
+const tictactoe = (moves) => {
+
+};
