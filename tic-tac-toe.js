2b6fba8d6010a6a218e0df9ff5eb39991b693be9
gkime <gkime@practice.com>
2022-04-23 19:03:53 -0700

    laid out subchallenges

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,11 +3,26 @@
 
 The first player A always places 'X' characters,
 while the second player B always places 'O' characters.
+Player A plays first.
 
 Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that
 the ith move will be played on grid[rowi][coli]. return the winner of the
 game if it exists (A or B). In case the game ends in a draw return "Draw". If
 there are still movements to play return "Pending". */
+
+// sub challenges:
+
+// board:
+// create a 2d grid using an array of arrays. Each subarray represents a box
+// in the array.
+
+
+// gameplay:
+// save coordinates of moves for player x and player o.
+// track whether a player has 3 in a row, horizontally, vertically, or diagonally.
+
+
+
 
 const tictactoe = (moves) => {
 
